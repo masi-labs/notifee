7df47619fde4074928f7c23896f8b9b6e4d31072
Masi Raymundo <smasiel.raymundo@gmail.com>
2026-02-15 00:49:18 +0100

    Adds additional details to setup

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 
+# Requirements file for production to install_requires
+with open('requirements/prod.txt', encoding='utf-8') as f:
+    REQUIREMENTS = f.read().splitlines()
+
 setup(
     name="notifee",
     version="0.1.0",
     description="Non-blocking HTTP notification client",
     package_dir={"": "src"},
+    package_data={'': ['py.typed']},
     packages=find_packages(where="src"),
     python_requires=">=3.10",
-    install_requires=[
-        "requests>=2.28.0",
-    ],
+    install_requires=REQUIREMENTS,
 )
